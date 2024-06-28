e39bed42d9d07a6b4eb34dcb86dacb66bb6061ff
Roman Mokhnatkin <kodiiiofc@gmail.com>
2024-06-28 12:09:56 +0500

    Домашнее задание IntellijIDE

---
+++ src/IntellijIDE/Main.java
@@ -0,0 +1,8 @@
+package IntellijIDE;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Роман Мохнаткин";
+        System.out.println("Привет, " + name);
+    }
+}
